c3a78784917c93d89d0f45b8d97f07d8f112f6cd
Somawatey <148378323+Somawatey@users.noreply.github.com>
2025-03-18 13:27:49 +0700

    Ex2_part2

--- lib/EX-2-START-CODE/model/post.dart
+++ lib/EX-2-START-CODE/model/post.dart
@@ -1,7 +1,18 @@
+import 'package:w6_providers_and_asynchronous_repositories/EX-2-START-CODE/dto/post_dto.dart';
+
 class Post {
   final int id;
   final String title;
   final String description;
 
   Post({required this.id, required this.title, required this.description});
+  
+  // Converts a PostDto object into a Post object.
+  factory Post.fromDto(PostDto dto) {
+    return Post(
+      id: dto.id,
+      title: dto.title,
+      description: dto.body,
+    );
+  }
 }
